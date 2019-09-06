fd8a066dabc1b50b75854be4fc9a2c7909148d96
sugi-chan <michaelsugimura@gmail.com>
2019-09-06 14:57:22 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ import sys
 from shutil import rmtree
 
 from setuptools import find_packages, setup, Command
+import setuptools
 
 NAME='multi-label-pigeon-jupyter'
 AUTHOR="Michael Sugimura"
@@ -20,72 +21,30 @@ REQUIRED = [
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = '\n' + f.read()
 
 about = {}
-with open(os.path.join(here, 'pigeon', '__version__.py')) as f:
+with open(os.path.join(here, 'multi_label_pigeon', '__version__.py')) as f:
     exec(f.read(), about)
 
 
-class PublishCommand(Command):
-    """Support setup.py publish."""
 
-    description = 'Build and publish the package.'
-    user_options = []
-    
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
-        except FileNotFoundError:
-            pass
-
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
-
-        self.status('Uploading the package to PyPi via Twine…')
-        os.system('twine upload dist/*')
-
-        sys.exit()
-
-setup(
-    name=NAME,
-    version=about['__version__'],
-    description=DESCRIPTION,
-    long_description=long_description,
-    author=AUTHOR,
-    author_email=EMAIL,
-    url=URL,
-    packages=['pigeon'],
-    install_requires=REQUIRED,
-    include_package_data=True,
-    license='Apache 2.0',
-    classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
-        'Framework :: Jupyter'
-    ],
-    # $ setup.py publish support. 
-    cmdclass={
-        'publish': PublishCommand,
-    },
-)
+setuptools.setup(
+     name=NAME,  
+     version=about['__version__'],
+     #scripts=['multi_label_pigeon'] ,
+     author=AUTHOR,
+     author_email="deepak.kumar.iet@gmail.com",
+     description=DESCRIPTION,
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url=URL,
+     packages=setuptools.find_packages(),
+     install_requires=REQUIRED,
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
